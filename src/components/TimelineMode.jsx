ed5b40e3251d01586db1fc17f959d01b59d5667e
oliveiralimadiogo <limaa.diogi@gmail.com>
2025-11-21 00:28:37 -0300

    [FIX]: Consertando alguns probleminhas de layout|

--- src/components/TimelineMode.jsx
+++ src/components/TimelineMode.jsx
@@ -1,9 +1,9 @@
-export default function TimelineNode() {
+export default function TimelineNode({isChecked}) {
   return (
     <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
       <div
         className="h-4 w-4 rounded-full border-2"
-        style={{ borderColor: "var(--primary)", background: "var(--bg)" }}
+        style={{ borderColor: "var(--primary)", background:  isChecked ? "var(--primary)" : "var(--bg)" }}
       />
     </div>
   );
